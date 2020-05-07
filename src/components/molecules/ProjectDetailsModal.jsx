5249c496923d87b9c70e0dae2499c463a06b28d9
Mark Artishuk <markyshuk@gmail.com>
2020-05-07 09:34:18 -0700

    Added scrollbar styling

--- src/components/molecules/ProjectDetailsModal.jsx
+++ src/components/molecules/ProjectDetailsModal.jsx
@@ -108,6 +108,23 @@ const InfoContainer = styled.div`
 
   overflow-x: hidden;
   overflow-y: auto;
+
+  &::-webkit-scrollbar-track {
+    // -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: inherit;
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    background-color: inherit;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #444444cc;
+  }
 `;
 
 const ButtonContainer = styled.div`
